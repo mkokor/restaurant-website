5fe160f7ca113a81fa58ae2b5b783901cbdf99e9
mkokor <matijakokorr@gmail.com>
2023-09-23 14:07:23 +0200

    contact section implemented

--- src/constants/images.js
+++ src/constants/images.js
@@ -18,6 +18,7 @@ import gallery1 from "../assets/images/gallery-1.png";
 import gallery2 from "../assets/images/gallery-2.png";
 import gallery3 from "../assets/images/gallery-3.png";
 import gallery4 from "../assets/images/gallery-4.png";
+import findUs from "../assets/images/find-us.png";
 
 export default {
   gericht,
@@ -40,4 +41,5 @@ export default {
   gallery2,
   gallery3,
   gallery4,
+  findUs,
 };
